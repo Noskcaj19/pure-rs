a4dd3da575ac26e066bc7804af79c5a9945fb9ee
Noskcaj <kcajdev@gmail.com>
2017-11-25 01:42:18 -0500

    Added git branch

--- src/precmd.rs
+++ src/precmd.rs
@@ -1,25 +1,38 @@
 use std::env;
+use git2::{self, Repository};
 
 use colored::*;
 
 
 const INVALID_PATH: &'static str = "???";
 
+fn render_git_status(repo: &Repository) -> Option<String> {
+    let shorthand: String = repo.head().unwrap().shorthand().unwrap().into();
+
+    Some(format!("{}", shorthand))
+}
+
 pub fn render() {
     let current_path: String = env::current_dir()
         .map(|p| p.to_str().unwrap().into())
         .unwrap_or(INVALID_PATH.into());
     // TODO: Add additional shortening?
-    let display_path = if let Some(home) = env::home_dir() {
+    let display_path: String = if let Some(home) = env::home_dir() {
         if let Some(home) = home.to_str() {
-            current_path.replace(home, "~")
+            current_path.clone().replace(home, "~")
         } else {
-            current_path
+            current_path.clone()
         }
     } else {
-        current_path
+        current_path.clone()
     };
 
+
+    let git_status = Repository::discover(current_path)
+        .ok()
+        .and_then(|repo| render_git_status(&repo))
+        .unwrap_or("".into());
+
     println!();
-    println!("{}", display_path.blue());
+    println!("{} {}", display_path.blue(), git_status);
 }
