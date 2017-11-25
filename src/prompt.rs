a3f69d616784bb01eed897304a89b5cc7144c32b
Noskcaj <kcajdev@gmail.com>
2017-11-25 09:10:13 -0500

    Changed back to shell colors to fix tab completion issue

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,16 +1,14 @@
 use clap::ArgMatches;
 
-use colored::*;
-
 pub fn render(sub_matches: &ArgMatches) {
     let previous_return_code = sub_matches
         .value_of("previous_return_code")
         .and_then(|r| r.parse().ok())
         .unwrap_or(0);
     let prompt = if previous_return_code != 0 {
-        "❯".red()
+        "%F{red}❯"
     } else {
-        "❯".blue()
+        "%F{blue}❯"
     };
-    print!("{} ", prompt);
+    print!("{}%f ", prompt);
 }
